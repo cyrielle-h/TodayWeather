f70f471366c3422ece056306afe0340bbee282a5
Cyrielle <cyrielle.hadamitzki@gmail.com>
2018-10-25 21:40:50 +0100

    added bootstrap and fontawesome

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// function isWindy (speed) {
+
+// 	if (speed > 5) {
+// 		alert('It is windy');
+
+// 	} else {
+// 		alert('it is not windy');
+// 	}
+// }
+
+// isWindy(10);
+// isWindy(4);
+// isWindy(-1);
+
+function isWindy (speed) {
+	if (speed > 5) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+let speed = 10;
+
+if (isWindy(10)) === true) {
+	alert('it is windy');
+} else } 
+	alert('it is not windy');
+}
