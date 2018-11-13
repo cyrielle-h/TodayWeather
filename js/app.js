7def00d9dcb02193e4fe0117a465a812a45d22f7
Cyrielle <cyrielle.hadamitzki@gmail.com>
2018-11-13 17:29:15 +0000

    Update the UI

--- js/app.js
+++ js/app.js
@@ -1,29 +1,35 @@
+// // function isWindy (speed) {
+
+// // 	if (speed > 5) {
+// // 		alert('It is windy');
+
+// // 	} else {
+// // 		alert('it is not windy');
+// // 	}
+// // }
+
+// // isWindy(10);
+// // isWindy(4);
+// // isWindy(-1);
+
 // function isWindy (speed) {
-
 // 	if (speed > 5) {
-// 		alert('It is windy');
-
+// 		return true;
 // 	} else {
-// 		alert('it is not windy');
+// 		return false;
 // 	}
 // }
 
-// isWindy(10);
-// isWindy(4);
-// isWindy(-1);
+// let speed = 10;
 
-function isWindy (speed) {
-	if (speed > 5) {
-		return true;
-	} else {
-		return false;
-	}
-}
+// if (isWindy(10)) === true) {
+// 	alert('it is windy');
+// } else } 
+// 	alert('it is not windy');
+// }
 
-let speed = 10;
 
-if (isWindy(10)) === true) {
-	alert('it is windy');
-} else } 
-	alert('it is not windy');
-}
+let element = document.querySelector('#min');
+element.addEventListener('click', function() {
+  alert('hi');
+});
