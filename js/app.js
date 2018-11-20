9cb7d4d82c963b4c846f19be4095aa8fdd0ab146
Cyrielle <cyrielle.hadamitzki@gmail.com>
2018-11-20 17:39:45 +0000

    WeatherAPI added

--- js/app.js
+++ js/app.js
@@ -1,35 +1,33 @@
-// // function isWindy (speed) {
+let temperature = document.querySelector('.temperature-now');
+let place = document.querySelector('.city');
+let pressure = document.querySelector('.detail-result__pressure');
+let wind = document.querySelector('.detail-result__wind');
+let humidity = document.querySelector('.detail-result__Humidity');
+let description = document.querySelector('#weather-description');
+let round = Math.round(temperature);
 
-// // 	if (speed > 5) {
-// // 		alert('It is windy');
+let icon          = document.querySelector('.weather__icon--today');
 
-// // 	} else {
-// // 		alert('it is not windy');
-// // 	}
-// // }
+let root   = 'https://api.openweathermap.org';
+let apiKey = '8682ae70ef78caa3eef84e53a0fb2349';
+let city   = 'Lisbon';
+let path   = 'data/2.5/weather';
 
-// // isWindy(10);
-// // isWindy(4);
-// // isWindy(-1);
+axios.get(root + '/' + path + '?q=' + city + '&appid=' + apiKey + '&units=metric')
+  .then(function(response) {
 
-// function isWindy (speed) {
-// 	if (speed > 5) {
-// 		return true;
-// 	} else {
-// 		return false;
-// 	}
-// }
+console.log(response);
+temperature.innerHTML = response.data.main.temp;
+place.innerHTML = 'in ' + response.data.name;
+pressure.innerHTML = response.data.main.pressure; 
+wind.innerHTML = response.data.wind.speed + ' km/h';
+humidity.innerHTML = response.data.main.humidity + ' %';
 
-// let speed = 10;
+// description.innerHTML = response.data.weather[0].main;
 
-// if (isWindy(10)) === true) {
-// 	alert('it is windy');
-// } else } 
-// 	alert('it is not windy');
-// }
+// let pathForecast = 'data/2.5/forecast';
 
+// axios.get(root + '/' + pathForecast + '?q=' + city + '&appid=' + apiKey + '&units=metric')
+//   .then(function(response) {
 
-let element = document.querySelector('#min');
-element.addEventListener('click', function() {
-  alert('hi');
-});
+ });
